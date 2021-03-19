7ceb161153a64bfc815e5c14dd97213a952126b8
mayconbalves <mayconmustaine@hotmail.com>
2021-03-19 17:23:35 -0300

    refactor(nav): change width and color of border

--- src/components/nav/styled.js
+++ src/components/nav/styled.js
@@ -1,12 +1,11 @@
 import styled from 'styled-components'
 
 export const Container = styled.div`
-  border-top: 2px solid #000;
-  border-bottom: 2px solid #000;
-  display: flex;
+  border-top: 2px solid #ff60e2;
+  border-bottom: 2px solid #ff60e2;
   height: 50px;
-  justify-content: space-around;
-  width: 100%;
+  width: 85%;
+  margin: 0 auto;
 
   @media (max-width: 780px) {
     justify-content: center;
@@ -15,13 +14,15 @@ export const Container = styled.div`
 
 export const UlStyled = styled.ul`
   display: flex;
+  justify-content: space-around;
 `
 
 export const NavItem = styled.li`
-  color: #000;
+  color: #ffc8f5;
   cursor: pointer;
   font-size: 2rem;
   padding: 10px;
+  text-shadow: 1px 1px 1px #000;
 
   &:hover {
     text-shadow: 1px 1px 1px #000;
